930ad253d9a9a96bafa4f31b2757d6f261027380
Adeyemo Samuel <adeyemosamuel@gmail.com>
2018-04-09 15:18:01 +0100

    newcorp

--- src/pages/addforma/addforma.ts
+++ src/pages/addforma/addforma.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, AlertController } from 'ionic-angular';
 import { AppdataProvider } from '../../providers/appdata/appdata';
+import { VerifyServiceProvider } from '../../providers/verify-service/verify-service';
 
 
 
@@ -10,6 +11,29 @@ import { AppdataProvider } from '../../providers/appdata/appdata';
   templateUrl: 'addforma.html',
 })
 export class AddformaPage {
+  fileupload: any;
+  valid: any;
+  info: any;
+  applied: any;
+  charge: any;
+  estimated: any;
+  transfer: any;
+  amount: any;
+  money: any;
+  payment: any;
+  ibpostalcode: any;
+  ibcountry: any;
+  ibstate: any;
+  ibcity: any;
+  ibaddress: any;
+  ibswiftcode: any;
+  ibname: any;
+  bbpostalcode: any;
+  bbstate: any;
+  bbcountry: any;
+  bbcity: any;
+  bbaddress: any;
+  bbname: any;
   selectedItem: any;
   baccount: any;
   bname: any;
@@ -33,6 +57,8 @@ export class AddformaPage {
   accountData: any=[];
 
   constructor(public navCtrl: NavController,
+    private alertCtrl: AlertController,
+    private verify: VerifyServiceProvider,
     private appdata: AppdataProvider,
     public modalCtrl: ModalController,
     public navParams: NavParams) {
@@ -87,5 +113,44 @@ export class AddformaPage {
 
   isReadonly() {
     return this.isReadonly;   //return true/false 
+  }
+
+  submitforma() {
+
+    if (!this.verify.submitforma(this.bname,
+this.baccount,
+this.name,
+this.bbswiftcode,
+this.bbname,
+this.bbaddress,
+this.bbcity,
+this.bbcountry,
+this.bbstate,
+this.bbpostalcode,
+this.ibname,
+this.ibswiftcode,
+this.ibaddress,
+this.ibcity,
+this.ibstate,
+this.ibcountry,
+this.ibpostalcode,
+this.payment,
+this.money,
+this.amount,
+this.transfer,
+this.estimated,
+this.charge,
+this.applied,
+this.info,
+this.valid,
+this.fileupload)) {
+      alert(this.verify.errorMessage);
+      this.alertCtrl.create({
+        subTitle: 'Message',
+        message: this.verify.errorMessage
+      }).present();
+     return false;
+     }
+    this.navCtrl.push('SuccessPage');
   }
 }
--- src/providers/verify-service/verify-service.ts
+++ src/providers/verify-service/verify-service.ts
@@ -9,86 +9,73 @@ export class VerifyServiceProvider {
 
   constructor(public http: HttpClient,
     private popCtrl: PopoverController,
-    private toastCtrl: ToastController,) {
+    private toastCtrl: ToastController, ) {
     console.log('Hello VerifyServiceProvider Provider');
   }
 
-  verifyNext(name, address, town, countries, states, phone,email,postcode, airline,ticket, route, identification, idnumber, comments): boolean {
-    if (name == null || name == '') {
-      this.errorMessage = 'Name is required'; 
-      return false;
-    }
+  submitforma(bname, baccount, name, bbswiftcode, bbname, bbaddress, bbcity,bbcountry, bbstate, bbpostalcode, ibname, ibswiftcode, ibaddress, ibcity, ibstate, ibcountry, ibpostalcode, payment, money, amount, transfer, estimated, charge, applied, info, valid, fileupload): boolean {
+    // if (bname == null || bname == '') {
+    //   this.errorMessage = 'Beneficiary name is required';
+    //   return false;
+    // }
 
-    if (address == null || address == '') {
-      this.errorMessage = 'Fill in the address';
-      return false;
-    }
+    // if (name == null || name == '') {
+    //   this.errorMessage = 'Account name is required';
+    //   return false;
+    // }
 
-    if (phone == '' || phone == null) {
-      this.errorMessage = 'Enter phone number';
-      return false;
-    }
+    // if (baccount == null || baccount == '') {
+    //   this.errorMessage = 'Account Number is required';
+    //   return false;
+    // }
 
-    if (email == '' || email == null) {
-      this.errorMessage = 'Enter Valid Email Address';
-      return false;
-    }
+    // if (bbswiftcode == null || bbswiftcode == '') {
+    //   this.errorMessage = 'Swift Code is required';
+    //   return false;
+    // }
 
-  
-    if (identification == '' || identification == null) {
-      this.errorMessage = 'Mode of identification is required';
-      return false;
-    }
+    // if (bbname == null || bbname == '') {
+    //   this.errorMessage = 'Beneficiary bank name is required';
+    //   return false;
+    // }
 
-    if (states == '' || states == null) {
-      this.errorMessage = 'Select a state';
-      return false;
-    }
+    // if (bbaddress == null || bbaddress == '') {
+    //   this.errorMessage = 'Beneficiary bank address is required';
+    //   return false;
+    // }
 
-    if (countries == '' || countries == null) {
-      this.errorMessage = 'Select a country';
-      return false;
-    }
+    // if (bbcity == null || bbcity == '') {
+    //   this.errorMessage = 'Beneficiary bank city is required';
+    //   return false;
+    // }
 
-    if (town == '' || town == null) {
-      this.errorMessage = 'Fill in the town';
-      return false;
-    }
+    // if (bbcountry == null || bbcountry == '') {
+    //   this.errorMessage = 'Beneficiary bank country is required';
+    //   return false;
+    // }
 
-    if (idnumber == '' || idnumber == null) {
-      this.errorMessage = 'Enter Valid ID Number';
-      return false;
-    }
+    // if (bbstate == null || bbstate == '') {
+    //   this.errorMessage = 'Beneficiary bank state is required';
+    //   return false;
+    // }
 
-    return true;
-  }
+    // if (bbpostalcode == null || bbpostalcode == '') {
+    //   this.errorMessage = 'Beneficiary bank postal code is required';
+    //   return false;
+    // }
 
-  verifyNext2(bname,baddress,bphone,baccount,bcountries,bpostcode): boolean{
-    if (bname == null || bname == '') {
-      this.errorMessage = 'Beneficiary name is required'; 
-      return false;
-    }
-
-    if (baddress == null || baddress == '') {
-      this.errorMessage = 'Fill in the address'; 
-      return false;
-    }
-    return true;
-  }
-
-  verifyNext3(payment,amount,charge,applied,info,valid,transfer,estimated): boolean{
     if (payment == null || payment == '') {
       this.errorMessage = 'Select Purpose of Payment'; 
       return false;
     }
 
     if (amount == null || amount == '') {
-      this.errorMessage = 'Select Amount'; 
+      this.errorMessage = 'Enter Amount'; 
       return false;
     }
 
     if (charge == null || charge == '') {
-      this.errorMessage = 'Select Charge Amount'; 
+      this.errorMessage = 'Select Charge Account'; 
       return false;
     }
 
@@ -96,7 +83,6 @@ export class VerifyServiceProvider {
       this.errorMessage = 'Enter amount applied in words'; 
       return false;
     }
-
 
     if (valid == null || valid == '') {
       this.errorMessage = 'Select Valid for Forex'; 
@@ -112,216 +98,16 @@ export class VerifyServiceProvider {
       this.errorMessage = 'Enter Estimated Debit Amount'; 
       return false;
     }
+
+    if (fileupload == null || fileupload == '') {
+      this.errorMessage = 'Attach files'; 
+      return false;
+    }
+
     return true;
   }
 
 
-  verifyFormNXP(name,address,city,states,phone,nepc,rcnumber,method,cfname, clname,caddress,countries): boolean{
-    if (name == null || name == '') {
-      this.errorMessage = 'Enter Full name'; 
-      return false;
-    }
-
-    if (address == null || address == '') {
-      this.errorMessage = 'Enter Address'; 
-      return false;
-    }
-
-    if (city == null || city == '') {
-      this.errorMessage = 'Select a city'; 
-      return false;
-    }
-
-    if (states == null || states == '') {
-      this.errorMessage = 'Select a state'; 
-      return false;
-    }
-
-
-    if (phone == null || phone == '') {
-      this.errorMessage = 'Enter Valid Phone Number'; 
-      return false;
-    }
-
-    if (nepc == null || nepc == '') {
-      this.errorMessage = 'Enter NEPC Reg No'; 
-      return false;
-    }
-
-    if (rcnumber == null || rcnumber == '') {
-      this.errorMessage = 'Enter RC Number'; 
-      return false;
-    }
-
-    if (method == null || method == '') {
-      this.errorMessage = 'Select Method of Payment'; 
-      return false;
-    }
-
-    if (cfname == null || cfname == '') {
-      this.errorMessage = 'Enter Consignee First Name'; 
-      return false;
-    }
-
-    if (clname == null || clname == '') {
-      this.errorMessage = 'Enter Consignee Last Name'; 
-      return false;
-    }
-
-    if (caddress == null || caddress == '') {
-      this.errorMessage = 'Enter Consignee Address'; 
-      return false;
-    }
-
-    if (countries == null || countries == '') {
-      this.errorMessage = 'Select Consignee Country'; 
-      return false;
-    }
-    return true;
-  }
-
-
-  verifyFormNXP2(fname,
-    lname,
-    address,
-    city,
-    states,
-    countries,
-    account,
-    domiciliary,
-    domammount,
-    formpurpose,
-    pincode,
-    charge,
-    prefix,
-    date): boolean{
-    if (account == null || account == '') {
-      this.errorMessage = 'Select Transfer Account'; 
-      return false;
-    }
-
-    if (formpurpose == null || formpurpose == '') {
-      this.errorMessage = 'Select Form Purpose Code'; 
-      return false;
-    }
-
-    if (pincode == null || pincode == '') {
-      this.errorMessage = 'Enter Pin Code'; 
-      return false;
-    }
-
-    if (charge == null || charge == '') {
-      this.errorMessage = 'Select Charge Account'; 
-      return false;
-    }
-
-
-    if (domammount == null || domammount == '') {
-      this.errorMessage = 'Enter Domiciliary Amount'; 
-      return false;
-    }
-
-    if (prefix == null || prefix == '') {
-      this.errorMessage = 'Select Prefix'; 
-      return false;
-    }
-
-   
-    return true;
-  }
-
-
-  verifyFormNXP3(sdate,
-    shipment,
-    pdate,
-    discharge,
-    proforma,
-    description,
-    quantity,
-    weight,
-    currency,
-    fob,
-    cf,
-    hs,
-    gross,
-    goods,
-    freight): boolean{
-    if (sdate == null || sdate == '') {
-      this.errorMessage = 'Select Shipment Date'; 
-      return false;
-    }
-
-    if (shipment == null || shipment == '') {
-      this.errorMessage = 'Select Expected Port of Shipment'; 
-      return false;
-    }
-
-    if (discharge == null || discharge == '') {
-      this.errorMessage = 'Select Port of destination/discharge'; 
-      return false;
-    }
-
-    if (description == null || description == '') {
-      this.errorMessage = 'Enter goods description'; 
-      return false;
-    }
-
-
-    if (quantity == null || quantity == '') {
-      this.errorMessage = 'Enter Total Quantity'; 
-      return false;
-    }
-
-    if (weight == null || weight == '') {
-      this.errorMessage = 'Enter Weight Value'; 
-      return false;
-    }
-
-
-    if (currency == null || currency == '') {
-      this.errorMessage = 'Select Currency'; 
-      return false;
-    }
-
-
-    if (fob == null || fob == '') {
-      this.errorMessage = 'Enter FOB Value'; 
-      return false;
-    }
-
-
-    if (cf == null || cf == '') {
-      this.errorMessage = 'Enter Total C & F Value'; 
-      return false;
-    }
-
-
-    if (hs == null || hs == '') {
-      this.errorMessage = 'Enter No of items expected'; 
-      return false;
-    }
-
-
-    if (gross == null || gross == '') {
-      this.errorMessage = 'Enter Gross Weight/Mass'; 
-      return false;
-    }
-
-
-    if (goods == null || goods == '') {
-      this.errorMessage = 'Enter Total Value of Goods'; 
-      return false;
-    }
-
-
-    if (freight == null || freight == '') {
-      this.errorMessage = 'Enter Total Freight Charges'; 
-      return false;
-    }
-
-   
-    return true;
-  }
 
 
   showToast(toastMsg, position) {
