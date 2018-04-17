377272c55a54107b883a972d7249bb593cc8540c
Adeyemo Samuel <adeyemosamuel@gmail.com>
2018-04-17 13:45:46 +0100

    newcorp

---
+++ src/pages/importlc/importlc.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ImportlcPage } from './importlc';
+
+@NgModule({
+  declarations: [
+    ImportlcPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ImportlcPage),
+  ],
+})
+export class ImportlcPageModule {}
---
+++ src/pages/importlc/importlc.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AppdataProvider } from '../../providers/appdata/appdata';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-importlc',
+  templateUrl: 'importlc.html',
+})
+export class ImportlcPage {
+  formnumber: any;
+  money:any;
+  amount:any;
+  mformsData: any = [];
+  moneyData:any=[];
+
+  constructor(public navCtrl: NavController,
+    private appdata: AppdataProvider,
+    public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.mformsData = this.appdata.getInfo();
+    this.moneyData=this.appdata.getInfo();
+  }
+
+}
