a927f91c214c3a81911a20025a921f1afbdecd67
adeyemosamuel <adeyemosamuel@gmail.com>
2018-08-06 16:03:23 +0100

    update

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,98 +1,97 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
-import { Storage } from '@ionic/storage';
-import { ServerServiceProvider } from '../../providers/server-service/server-service';
-import { ToastController } from 'ionic-angular/components/toast/toast-controller';
-import { ControllerProvider } from '../../providers/controller/controller';
-
-
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  LoadingController,
+  AlertController
+} from "ionic-angular";
+import { Storage } from "@ionic/storage";
+import { ServerServiceProvider } from "../../providers/server-service/server-service";
+import { ToastController } from "ionic-angular/components/toast/toast-controller";
+import { ControllerProvider } from "../../providers/controller/controller";
 
 @IonicPage()
 @Component({
-    selector: 'page-login',
-    templateUrl: 'login.html',
+  selector: "page-login",
+  templateUrl: "login.html"
 })
 export class LoginPage {
-    userName: string = '';
-    passWord: string = '';
-    disableButton: boolean = false;
+  cifid: string = "";
+  userName: string = "";
+  passWord: string = "";
+  disableButton: boolean = false;
 
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController,
+    public control: ControllerProvider,
+    private store: Storage,
+    private server: ServerServiceProvider
+  ) {}
 
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad LoginPage");
+  }
 
-    constructor(public navCtrl: NavController,
-        public navParams: NavParams,
-        private alertCtrl: AlertController,
-        private toastCtrl: ToastController,
-        public control: ControllerProvider,
-        private store: Storage,
-        private server: ServerServiceProvider) {
-
-
+  validateInput(val) {
+    if (this.userName.trim != null && this.passWord != "") {
+      this.disableButton = false;
+    } else {
+      this.disableButton = true;
     }
+  }
 
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad LoginPage');
-    }
+  async Login() {
+    let loader = this.control.loadCtrl("Please wait...");
+    loader.present();
 
-    validateInput(val){
-      if (this.userName.trim != null && this.passWord != ''){
-        this.disableButton = false;
-      }else{
-        this.disableButton = true;
+    let body = {
+      userName: this.userName,
+      passWord: this.passWord
+    };
+
+    try {
+      let response = await this.server.processData(body, "/user/Signin");
+      console.log(response);
+      if (response.status === "00") {
+        this.control.toastCtrl(response.message, "middle", false);
+        this.store.set("token", response.data.token);
+        this.navCtrl.push("TabsPage");
+      } else if (response.status == "99") {
+        this.control.toastCtrl(response.message, "middle", false);
+      } else if (response.status == "100") {
+        this.control.toastCtrl(response.message, "middle", false);
+      } else {
+        this.control.toastCtrl(
+          "Login Failed. Please try again",
+          "middle",
+          false
+        );
       }
+    } catch (err) {
+      console.log(err);
+      // this.createToaster('Unable to login. Try again later.');
     }
 
-    async Login() {
-        let loader = this.control.loadCtrl('Please wait...');
-        loader.present();
+    loader.dismiss();
+  }
 
-        let body = {
-            userName: this.userName,
-            passWord:this.passWord
-        };
+  forgot() {
+    this.navCtrl.push("ForgotPage");
+  }
 
-        try {
-            let response = await this.server.processData(body, '/user/Signin');
-            console.log(response);
-            if (response.status === '00') {
-              this.control.toastCtrl(response.message, 'middle', false);
-                this.store.set('token', response.data.token);
-                this.navCtrl.push('TabsPage');
-            } else if (response.status == '99') {
-              this.control.toastCtrl(response.message, 'middle', false);
-            } else if(response.status == '100'){
-              this.control.toastCtrl(response.message, 'middle', false);
-            } else{
-              this.control.toastCtrl("Login Failed. Please try again", 'middle', false);
-            }
-
-
-        } catch (err) {
-            console.log(err);
-        // this.createToaster('Unable to login. Try again later.');
-        }
-
-        loader.dismiss();
-    }
-
-    forgot(){
-        this.navCtrl.push('ForgotPage');
-    }
-
-
-     // Toast Creation Method
-  createToaster(msg){
-
+  // Toast Creation Method
+  createToaster(msg) {
     let toaster = this.toastCtrl.create({
       message: msg,
       showCloseButton: true,
-      closeButtonText: 'OK',
-      cssClass: 'toastStyle',
-      position: 'middle'
+      closeButtonText: "OK",
+      cssClass: "toastStyle",
+      position: "middle"
     });
     toaster.present();
   }
-
-
-
 }
